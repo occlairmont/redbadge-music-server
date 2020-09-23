000851b6f289ee71c0a473daa56e584d365a256f
Kayla Bullard <kaylabullard79@gmail.com>
2020-09-22 20:09:20 -0400

    Finished Music Controller and Model

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ let express = require('express');
 let app = express();
 let sequelize = require('./db');
 
+let music = require('./controllers/musiccontroller');
+
 sequelize.sync();
 app.use(require('./middleware/headers'));
 
 app.use(express.json());
+
+app.use('/music', music);
 
 
 app.listen(3001, function(){
