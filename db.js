a8d3b74567e056fd6f0cd4303e748c1e469c0e9c
occlairmont <occlairmont@gmail.com>
2020-10-25 19:17:05 -0400

    first commit

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("redbadge-music-logs", "postgres", "password", {
-    host: "localhost",
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
 })
 sequelize.authenticate().then(
